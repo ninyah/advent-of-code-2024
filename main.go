193abe1c72a30f7bdb23cc66daf282088ae5799a
ninyah <hi@ninyah.com>
2024-12-02 11:10:47 -0500

    Finish problem 1

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	// Just add new problem functions to the array, as you complete them
 	// Because you can't store functions in arrays, we had to use the any type
-	problems := []any {
+	problems := []any{
 		nil,
 		internal.SolveProblem1,
 		internal.SolveProblem2,
@@ -20,7 +20,7 @@ func main() {
 	// Generate an optional command line flag to run a specific problem, defaulting to the latest
 	problemNumber := flag.Int("p", problemCount, "The problem number to be run")
 	flag.Parse()
-	
+
 	// Grab the function from the array and run it, if possible
 	if problems[*problemNumber] != nil {
 		// Convert our any type back to a function and call it
